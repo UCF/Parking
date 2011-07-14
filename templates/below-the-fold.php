d024b0f87839ac910c7fbdde3efe7e96a842bac7
Douglas Beck <dbeck@douglasbeck.com>
2011-07-14 17:26:16 -0400

    updating below fold

--- templates/below-the-fold.php
+++ templates/below-the-fold.php
@@ -1,12 +1,42 @@
 <div id="below-fold">
-	<?php while(have_posts()): the_post();?>
-	<article>
-		<h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
-		<div class="meta">
-			<span class="date">Posted on <?php the_time("F j, Y");?></span>
-			<span class="author">by <?php the_author_posts_link();?></span>
+	
+	<div class="span-7">
+		<h2>Parking Updates</h2>
+		<?php while(have_posts()): the_post();?>
+		<article>
+			<h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+			<p><a href="<?php the_permalink();?>"><?php the_excerpt();?></a></p>
+		</article>
+		<?php endwhile;?>
+	</div>
+	
+	<div class="span-8">
+		<h2>Traffic Updates</h2>
+		<article>
+			<h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+			<p><a href="<?php the_permalink();?>"><?php the_excerpt();?></a></p>
+		</article>
+	</div>
+	
+	<div class="span-9 last">
+		<div id="search">
+			Search
 		</div>
-		<?php the_excerpt();?>
-	</article>
-	<?php endwhile;?>
+		
+		<div class="more">
+			<h3>More Resources</h3>
+			<ul>
+				<li><a>Bicycles</a></li>
+				<li><a>Carpooling</a></li>
+				<li><a>Disability Parking</a></li>
+				<li><a>Escort Services</a></li>
+				<li><a>Golf Carts</a></li>
+				<li><a>KnightLynx</a></li>
+				<li><a>Park ‘n’ Ride</a></li>
+				<li><a>Parking at UCF</a></li>
+				<li><a>Service Vehicles</a></li>
+			</ul>
+		</div>
+	</div>
+		
 </div>
