cabd7b24f6bddfd875f852bd6a5bf16d4b94d611
Douglas Beck <dbeck@douglasbeck.com>
2011-07-18 10:29:03 -0400

    implementing fonts

--- sidebar.php
+++ sidebar.php
@@ -4,18 +4,18 @@
 	<div class="side-nav">
 		<h2>Find Out More</h2>
 		<ul>
-			<li><a href="">Pay a Citation</a></li>
-			<li><a href="">Parking Maps</a></li>
-			<li><a href="">Shuttle Schedules</a></li>
+			<li><a class="light" href="">Pay a Citation</a></li>
+			<li><a class="light" href="">Parking Maps</a></li>
+			<li><a class="light" href="">Shuttle Schedules</a></li>
 		</ul>
 	</div>
 	
 	<div class="side-nav">
 		<h2>Parking Permit Information</h2>
 		<ul>
-			<li><a href="">Visitor Permits</a></li>
-			<li><a href="">Students Permits</a></li>
-			<li><a href="">Employee Permits</a></li>
+			<li><a class="light" href="">Visitor Permits</a></li>
+			<li><a class="light" href="">Students Permits</a></li>
+			<li><a class="light" href="">Employee Permits</a></li>
 		</ul>
 	</div>
 	
--- templates/below-the-fold.php
+++ templates/below-the-fold.php
@@ -1,7 +1,7 @@
 <div id="below-fold">
 	
 	<div class="span-7">
-		<h2>Parking Updates</h2>
+		<h2 class="rock">Parking Updates</h2>
 		<?php while(have_posts()): the_post();?>
 		<article>
 			<h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
@@ -11,7 +11,7 @@
 	</div>
 	
 	<div class="span-8">
-		<h2>Traffic Updates</h2>
+		<h2 class="rock">Traffic Updates</h2>
 		<article>
 			<h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
 			<p><a href="<?php the_permalink();?>"><?php the_excerpt();?></a></p>
@@ -24,17 +24,17 @@
 		</div>
 		
 		<div class="more">
-			<h3>More Resources</h3>
+			<h3 class="rock">More Resources</h3>
 			<ul>
-				<li><a>Bicycles</a></li>
-				<li><a>Carpooling</a></li>
-				<li><a>Disability Parking</a></li>
-				<li><a>Escort Services</a></li>
-				<li><a>Golf Carts</a></li>
-				<li><a>KnightLynx</a></li>
-				<li><a>Park ‘n’ Ride</a></li>
-				<li><a>Parking at UCF</a></li>
-				<li><a>Service Vehicles</a></li>
+				<li><a href="" class="light">Bicycles</a></li>
+				<li><a href="" class="light">Carpooling</a></li>
+				<li><a href="" class="light">Disability Parking</a></li>
+				<li><a href="" class="light">Escort Services</a></li>
+				<li><a href="" class="light">Golf Carts</a></li>
+				<li><a href="" class="light">KnightLynx</a></li>
+				<li><a href="" class="light">Park ‘n’ Ride</a></li>
+				<li><a href="" class="light">Parking at UCF</a></li>
+				<li><a href="" class="light">Service Vehicles</a></li>
 			</ul>
 		</div>
 	</div>
