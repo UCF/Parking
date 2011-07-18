aca5f3ea2db5a97e013778fe608a1ec36fa8448b
Douglas Beck <dbeck@douglasbeck.com>
2011-07-18 15:58:58 -0400

    styling footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-			<div id="footer" class="span-24 last">
+			<div id="footer" class="span-24 last light">
 				<?=get_menu('footer-menu', 'menu horizontal', 'footer-menu')?>
 			</div>
 		</div><!-- #blueprint-container -->
