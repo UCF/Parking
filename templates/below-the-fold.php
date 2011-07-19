7e225d19272c71ced4491b76359c6ffded5148af
Douglas Beck <dbeck@douglasbeck.com>
2011-07-19 15:49:26 -0400

    applying categories to traffic and parking updates

--- templates/below-the-fold.php
+++ templates/below-the-fold.php
@@ -2,20 +2,38 @@
 	
 	<div class="span-7">
 		<h2 class="rock">Parking Updates</h2>
-		<?php while(have_posts()): the_post();?>
+		<?php
+			$cat = get_category_by_slug('parking');
+			if(!$cat->term_id) echo "<!-- Parking Category Missing -->";
+			$posts = query_posts( array ( 'cat' => $cat->term_id, 'posts_per_page' => 1 ) );
+			if($cat->term_id && count($posts) > 0):
+				the_post();
+		?>
 		<article>
 			<h3><a class="bold" href="<?php the_permalink();?>"><?php the_title();?></a></h3>
 			<p><a href="<?php the_permalink();?>"><?php the_excerpt();?></a></p>
 		</article>
-		<?php endwhile;?>
+		<?php else: ?>
+		<p>There are no parking updates.</p>
+		<?php endif; ?>
 	</div>
 	
 	<div class="span-8">
 		<h2 class="rock">Traffic Updates</h2>
+		<?php
+			$cat = get_category_by_slug('traffic');
+			if(!$cat->term_id) echo "<!-- Traffic Category Missing -->";
+			$posts = query_posts( array ( 'cat' => $cat->term_id, 'posts_per_page' => 1 ) );
+			if($cat->term_id && count($posts) > 0):
+				the_post();
+		?>
 		<article>
 			<h3><a class="bold" href="<?php the_permalink();?>"><?php the_title();?></a></h3>
 			<p><a href="<?php the_permalink();?>"><?php the_excerpt();?></a></p>
 		</article>
+		<?php else: ?>
+		<p>There are no traffic updates.</p>
+		<?php endif; ?>
 	</div>
 	
 	<div class="span-9 last">
