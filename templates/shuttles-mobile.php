1e767ee6d40f9eaf552d55f219cb0ab1e765e3f3
Douglas Beck <dbeck@douglasbeck.com>
2011-11-18 09:44:55 -0500

    shuttles mobile template

---
+++ templates/shuttles-mobile.php
@@ -0,0 +1,11 @@
+<?php
+$page   = get_page_by_path('shuttles');
+$content  = $page->post_content;
+$content  = apply_filters('the_content', $content);
+$content  = str_replace(']]>', ']]>', $content);
+
+// clean up classes and remove images
+$content  = preg_replace('/\s?class="[^"]+"/', '', $content);
+$content  = preg_replace('/<img[^>]+>/', '', $content);
+
+echo $content;
