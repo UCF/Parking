83f85cca986b38469828b2e19bfc70a78331143c
Brandon Groves <brandon.groves@gmail.com>
2013-12-06 15:18:43 -0500

    theme url change

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 # Define stuff
-define('THEME_URL', get_bloginfo('stylesheet_directory'));
-define('THEME_DIR', get_stylesheet_directory_uri());
+define('THEME_URL', get_stylesheet_directory_uri());
+define('THEME_DIR', get_stylesheet_directory());
 define('THEME_STATIC_URL', THEME_URL.'/static');
 define('THEME_IMG_URL', THEME_STATIC_URL.'/img');
 define('THEME_JS_URL', THEME_STATIC_URL.'/js');
